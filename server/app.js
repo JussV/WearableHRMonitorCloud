11340b03635828d94a1a749dca482579de85a555
Joana Veljanoska <joana.veljanoska@gmail.com>
2017-11-17 07:28:29 +0100

    Schedule cron jobs to automatically run pre-process data in heartrate mapreduce collections

--- server/app.js
+++ server/app.js
@@ -39,89 +39,104 @@ function startServer() {
   });
 
   app.listen(5000, function() {
-    //execute on every 5 hours
-    /*schedule.scheduleJob('0 *!/5 * * * *', function() {
-      let intervalInMinutes = 15;
-
-      var o = {},
-        self = this;
-      o.map = function() {
-        var minuteSubset;
-        var mins = this.date.getMinutes();
-        var numberOfMinuteSubsets = 60 / 15;
-        var i;
-        for(i = 1; i <= numberOfMinuteSubsets; i++) {
-          if(i * 15 > mins) {
-            minuteSubset = (i - 1) * 15;
-            break;
+    //execute at 5 AM every day
+    let intervals = ['15', '30'];
+    let modelArr = [models.MapReducedHeartRates15, models.MapReducedHeartRates30];
+    let collections = ['heartrates-mapreduce-15', 'heartrates-mapreduce-30'];
+    let delay = 0;
+    let runJobAtHour = 5;
+    let runJobAtMin = 0;
+    async.eachOfSeries(intervals, function(item, index, finalCallback) {
+      runJobAtMin = runJobAtMin + delay;
+      if(runJobAtMin > 60) {
+        runJobAtHour = runJobAtHour + 1;
+        runJobAtMin = runJobAtMin - 60;
+      }
+      schedule.scheduleJob(item, {hour: runJobAtHour, minute: runJobAtMin}, function() {
+        let intervalInMin = parseInt(item);
+        var o = {},
+          self = this;
+        o.map = function() {
+          var minuteSubset;
+          var mins = this.date.getMinutes();
+          var numberOfMinuteSubsets = 60 / interval;
+          var i;
+          for(i = 1; i <= numberOfMinuteSubsets; i++) {
+            if(i * interval > mins) {
+              minuteSubset = (i - 1) * interval;
+              break;
+            }
           }
-        }
-        var time_at_minute = new Date(
-          this.date.getFullYear(),
-          this.date.getMonth(),
-          this.date.getDate(),
-          this.date.getHours(),
-          minuteSubset);
+          var time_at_minute = new Date(
+            this.date.getFullYear(),
+            this.date.getMonth(),
+            this.date.getDate(),
+            this.date.getHours(),
+            minuteSubset);
 
-        emit({time_at_minute, device: this.device, upid: this.uniquePhoneId}, {
-          device: this.device,
-          heartrate: this.value,
-          date: this.date
-        });
-      };
-      o.reduce = function(key, values) {
-        var heartRateTotal = 0.0;
-        var count = values.length;
-        var heartrate = 0.0;
-        values.forEach(function(value) {
-          heartRateTotal = heartRateTotal + value.heartrate;
-        });
-
-        var result = {
-          upid: key.upid,
-          date: key.time_at_minute,
-          heartrate: heartRateTotal / count,
-          dateToMilliSec: key.time_at_minute.getTime(),
-          device: key.device,
-         // values: values
+          emit({time_at_minute, device: this.device, upid: this.uniquePhoneId}, {
+            device: this.device,
+            heartrate: this.value,
+            date: this.date
+          });
         };
-        return result;
-      };
+        o.reduce = function(key, values) {
+          var heartRateTotal = 0.0;
+          var count = values.length;
+          var heartrate = 0.0;
+          values.forEach(function(value) {
+            heartRateTotal = heartRateTotal + value.heartrate;
+          });
 
-      async.waterfall([
-        function(callback) {
-          models.MapReducedHeartRates15.findOne({}, {}, { sort: {'_id.time_at_minute': -1} }, function(err, lastMappedRecord) {
-            if(err) {
-              return callback(err);
-            } else {
-              o.out = {
-                query: {
-                  value: {$gt: 0},
-                  date: {$gt: new Date(lastMappedRecord.toObject().value.dateToMilliSec + 15 * 60 * 1000)}
-                },
-                reduce: 'heartrates-mapreduce-15',
-                scope: {'interval': 15}
-              };
-              return callback(null, lastMappedRecord);
-            }
-          });
-        },
-        function(lastMappedRecord, callback) {
-          models.Heartrate.mapReduce(o, function(err, results) {
-            if(err) {
-              return callback(err);
-            } else {
-              return callback(null, results);
-            }
-          });
-        }
-      ], function(err, result) {
-        if(err) {
-       //   console.log('error:' + err);
-        }
-        console.log(result);
+          var result = {
+            upid: key.upid,
+            date: key.time_at_minute,
+            heartrate: heartRateTotal / count,
+            dateToMilliSec: key.time_at_minute.getTime(),
+            device: key.device,
+            // values: values
+          };
+          return result;
+        };
+
+        async.waterfall([
+          function(callback) {
+            modelArr[index].findOne({}, {}, { sort: {'_id.time_at_minute': -1} }, function(err, lastMappedRecord) {
+              if(err) {
+                return callback(err);
+              } else {
+                return callback(null, lastMappedRecord);
+              }
+            });
+          },
+          function(lastMappedRecord, callback) {
+            let scope = {}
+            scope.interval = intervalInMin;
+            let dateCondition = {$gt: new Date(lastMappedRecord.toObject().value.dateToMilliSec + intervalInMin * 60 * 1000)};
+            o.query = {
+              value: {$gt: 0},
+              date: dateCondition,
+            };
+            o.out = { reduce: collections[index]};
+            o.scope = {interval: intervalInMin};
+            models.Heartrate.mapReduce(o, function(err, results) {
+              if(err) {
+                return callback(err);
+              } else {
+                return callback(null, results);
+              }
+            });
+          }
+        ], function(err, result) {
+          if(err) {
+            //   console.log('error:' + err);
+          }
+          console.log(result);
+        });
+        delay = delay + 15;
+        finalCallback();
       });
-    });*/
+    });
   });
 }
 
